3f0ccda7d3857787460cb3c482d37b4ba95f0569
Zander <CredibleOpossum@protonmail.com>
2023-04-23 00:33:31 -0400

    add source

---
+++ SaveEditor/src/lib.rs
@@ -0,0 +1,63 @@
+use aes::Aes256;
+use base64::{engine::general_purpose, Engine as _};
+use block_modes::block_padding::Pkcs7;
+use block_modes::{BlockMode, Cbc};
+use serde_json::Value;
+use std::error;
+use std::path::Path;
+use winreg::enums::HKEY_CURRENT_USER;
+use winreg::enums::REG_BINARY;
+use winreg::RegKey;
+use winreg::RegValue;
+
+const SAVE_LOCATION: &str = "Software\\Nussygame\\Buriedbornes";
+const SAVE_NAME: &str = "UqgwrrCj_h2961706936";
+const SAVE_KEY: &[u8] = b"a5VckiKFIP76hvPnoae9PRZ5x1PzUiVA";
+const IV_SIZE: usize = 16;
+
+type Aes256Cbc = Cbc<Aes256, Pkcs7>;
+
+fn decrypt(cipher_bytes: &[u8], iv: &[u8]) -> Result<String, Box<dyn error::Error>> {
+    let mut encrypted_data = cipher_bytes.to_owned();
+    let cipher = Aes256Cbc::new_from_slices(SAVE_KEY, iv)?;
+    cipher.decrypt(&mut encrypted_data).unwrap().to_vec();
+    Ok(String::from_utf8(encrypted_data)?)
+}
+
+fn encrypt(plain_text: &[u8], iv: &[u8]) -> Result<Vec<u8>, Box<dyn error::Error>> {
+    let bytes = plain_text.to_owned();
+    let cipher = Aes256Cbc::new_from_slices(SAVE_KEY, iv)?;
+    Ok(cipher.encrypt_vec(&bytes))
+}
+
+pub fn savefile_read() -> (Value, Vec<u8>, u8) {
+    let hklm = RegKey::predef(HKEY_CURRENT_USER);
+    let data_location = hklm.open_subkey(SAVE_LOCATION).unwrap();
+    let mut bytes = data_location.get_raw_value(SAVE_NAME).unwrap().bytes;
+    let remainder = bytes.pop().unwrap(); // remove remainder
+    let base64_string = String::from_utf8(bytes).unwrap(); // decode
+    let decoded = general_purpose::STANDARD.decode(base64_string).unwrap();
+    let (iv, cipher_bytes) = decoded.split_at(IV_SIZE); // decrypt
+    let decrypted = decrypt(cipher_bytes, iv).unwrap();
+    let (json_data, _unknown_data) = decrypted.split_at(decrypted.rfind('}').unwrap() + 1);
+    let json: Value = serde_json::from_str(json_data).unwrap();
+    (json, iv.to_vec(), remainder)
+}
+
+pub fn savefile_writejson(json: Value, iv: Vec<u8>, remainder: u8) {
+    let hklm = RegKey::predef(HKEY_CURRENT_USER);
+    let bytes: Vec<u8> = json.to_string().try_into().unwrap();
+    let mut re_encrypted = iv.to_vec();
+    re_encrypted.extend(encrypt(&bytes, &iv).unwrap()); // encrypt
+    let binding = general_purpose::STANDARD.encode(re_encrypted); // encode
+    let mut stored_data = binding.as_bytes().to_vec();
+    stored_data.push(remainder);
+    let raw_data = stored_data;
+    let reg_data = RegValue {
+        vtype: REG_BINARY,
+        bytes: raw_data,
+    };
+    let path = Path::new(SAVE_LOCATION);
+    let (key, _disp) = hklm.create_subkey(path).unwrap();
+    key.set_raw_value(SAVE_NAME, &reg_data).unwrap();
+}
---
+++ src/main.rs
@@ -0,0 +1,144 @@
+use bb_save_editor;
+use eframe::egui;
+use egui::Ui;
+use serde_json::Value;
+
+fn main() -> Result<(), eframe::Error> {
+    env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
+    let options = eframe::NativeOptions {
+        initial_window_size: Some(egui::vec2(800.0, 800.0)),
+        ..Default::default()
+    };
+    eframe::run_native(
+        "BuriedBornes Save Editor",
+        options,
+        Box::new(|_cc| Box::<AppData>::default()),
+    )
+}
+
+struct AppData {
+    save_data_exists: bool,
+    save_data: Value,
+    save_iv: Vec<u8>,
+    save_carry: u8,
+
+    golden_shard_count: i32,
+    soul_stone_count: i32,
+
+    json_text: String,
+    vaild_json: bool,
+}
+
+impl Default for AppData {
+    fn default() -> Self {
+        Self {
+            save_data_exists: false,
+            save_data: "".into(),
+            save_iv: vec![],
+            save_carry: 0,
+
+            soul_stone_count: 0,
+            golden_shard_count: 0,
+
+            json_text: "".to_string(),
+            vaild_json: true,
+        }
+    }
+}
+
+fn soul_stone_ui(state: &mut AppData, ui: &mut Ui) {
+    ui.label("Soul Stones");
+    let slider = ui.add(
+        egui::Slider::new(&mut state.soul_stone_count, 0..=999_999_999)
+            .text("Amount")
+            .logarithmic(true),
+    );
+    if slider.changed() {
+        let paid = state.save_data["Soulstone"]["paid"].as_f64().unwrap() as i32;
+        let used = state.save_data["Soulstone"]["used"].as_f64().unwrap() as i32;
+        state.save_data["Soulstone"]["gain"] = (state.soul_stone_count + paid + used).into();
+        state.json_text = serde_json::to_string_pretty(&state.save_data).unwrap();
+        state.vaild_json = true;
+    }
+}
+
+fn golden_shard_ui(state: &mut AppData, ui: &mut Ui) {
+    ui.label("Golden Shard");
+    let slider = ui.add(
+        egui::Slider::new(&mut state.golden_shard_count, 0..=999_999_999)
+            .text("Amount")
+            .logarithmic(true),
+    );
+    if slider.changed() {
+        let paid = state.save_data["GoldenShard"]["paid"].as_f64().unwrap() as i32;
+        let used = state.save_data["GoldenShard"]["used"].as_f64().unwrap() as i32;
+        state.save_data["GoldenShard"]["gain"] = (state.golden_shard_count + paid + used).into();
+        state.json_text = serde_json::to_string_pretty(&state.save_data).unwrap();
+        state.vaild_json = true;
+    }
+}
+
+fn load_save(state: &mut AppData) {
+    let data = bb_save_editor::savefile_read();
+    state.save_data = data.0;
+    state.save_iv = data.1;
+    state.save_carry = data.2;
+
+    let earned = state.save_data["GoldenShard"]["gain"].as_f64().unwrap() as i32;
+    let paid = state.save_data["GoldenShard"]["paid"].as_f64().unwrap() as i32;
+    let used = state.save_data["GoldenShard"]["used"].as_f64().unwrap() as i32;
+    state.golden_shard_count = earned - paid - used;
+
+    let earned = state.save_data["Soulstone"]["gain"].as_f64().unwrap() as i32;
+    let paid = state.save_data["Soulstone"]["paid"].as_f64().unwrap() as i32;
+    let used = state.save_data["Soulstone"]["used"].as_f64().unwrap() as i32;
+    state.soul_stone_count = earned - paid - used;
+
+    state.json_text = serde_json::to_string_pretty(&state.save_data).unwrap();
+    state.save_data_exists = true;
+}
+
+impl eframe::App for AppData {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ctx.set_pixels_per_point(1.5);
+            if ui.button("Load save").clicked() {
+                load_save(self);
+            }
+            ui.separator();
+            if self.save_data_exists {
+                if ui.button("Save").clicked() {
+                    bb_save_editor::savefile_writejson(
+                        self.save_data.clone(),
+                        self.save_iv.clone(),
+                        self.save_carry,
+                    );
+                }
+                soul_stone_ui(self, ui);
+                golden_shard_ui(self, ui);
+                ui.separator();
+                ui.label("Other data (you can modify, make backups)");
+                if self.vaild_json {
+                    ui.label("Data is currently vaild JSON");
+                } else {
+                    ui.label("Data is currently NOT vaild JSON");
+                }
+                egui::ScrollArea::vertical()
+                    .id_source("first")
+                    .show(ui, |ui| {
+                        let text_area = ui.text_edit_multiline(&mut self.json_text);
+                        if text_area.changed() {
+                            let json_data = serde_json::from_str(&self.json_text);
+                            match json_data {
+                                Ok(data) => {
+                                    self.vaild_json = true;
+                                    self.save_data = data;
+                                }
+                                Err(_) => self.vaild_json = false,
+                            }
+                        }
+                    });
+            };
+        });
+    }
+}
